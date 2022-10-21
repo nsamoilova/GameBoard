0021877f04dacfc6389b40c83ce72e94d67ad106
Nasty Samoilova <nsamoilov.an@gmail.com>
2022-10-21 17:47:30 +0300

    game

---
+++ index.js
@@ -0,0 +1,33 @@
+const board = document.querySelector('#board')
+const colors = ['#87ece7', '#87beec', '#8e87ec', '#aa87ec', '#ec87e7', '#a4ec87', '#ee8888']
+const SQUARES_NUMBER = 500
+
+for(let i = 0; i < SQUARES_NUMBER; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+        setColor(square))
+
+    square.addEventListener('mouseleave', () => 
+        removeColor(square))    
+    
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+   const index = Math.floor(Math.random() * colors.length)
+   return colors[index]
+}
